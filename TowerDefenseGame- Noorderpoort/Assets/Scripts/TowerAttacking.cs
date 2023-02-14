88231f0a3b91fcf5688f5b2b1f816c82b318868d
RudoVerschuren <rpverschuren@st.noorderpoort.nl>
2023-02-14 15:49:32 +0100

    Starting on tower attacking

---
+++ TowerDefenseGame- Noorderpoort/Assets/Scripts/TowerAttacking.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAttacking : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject projectile;
+    public Transform firepoint;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //target = idk
+
+    }
+}
