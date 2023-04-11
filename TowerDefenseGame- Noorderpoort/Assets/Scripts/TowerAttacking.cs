515a6d70c2ac5a1ee194991589a6d6cd7695a7e7
MK-v5 <mksomna@st.noorderpoort.nl>
2023-04-11 11:32:59 +0200

    updatedTOWERATATACK

--- TowerDefenseGame- Noorderpoort/Assets/Scripts/TowerAttacking.cs
+++ TowerDefenseGame- Noorderpoort/Assets/Scripts/TowerAttacking.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 using UnityEngine.UIElements;
 
 public class TowerAttacking : MonoBehaviour
@@ -10,26 +11,31 @@ public class TowerAttacking : MonoBehaviour
     public float timeStunned = 5;
     float stunTime = 0;
     bool stunned = false;
-    //hoe lang de tower's stunned blijven (niet attacken)
+    public GameObject target;
     public GameObject projectile;
+    public GameObject rangeCircle;
     public Transform firePoint;
     public float fireRate;
     public float timeUntilBullet;
-    public float projectileSpeed;
     public float damage;
-    //firepoint (projectile spawn) en projectile damage/speed
-    public GameObject rangeCircle;
-    public GameObject target;
     public float range;
+    public float projectileSpeed;
     public bool enemyInRange = false;
-    //rangeCircle (hoe ver de towers schieten) en target (Enemy)
+    GameObject bulletEmpty;
+    [SerializeField] private bool defaultTower = true;
+    [SerializeField] private bool IforgotTheName;
     void Start()
     {
-        rangeCircle.transform.localScale = new Vector3(range , rangeCircle.transform.localScale.y, range);
+        bulletEmpty = GameObject.Find("BulletEmpty");
+        rangeCircle.transform.localScale = new Vector3(range, rangeCircle.transform.localScale.y, range);
     }
 
     void Update()
     {
+        if (target == null)
+        {
+            enemyInRange = false;
+        }
         if (Input.GetKey(KeyCode.H))
         {
             health--;
@@ -47,23 +53,34 @@ public class TowerAttacking : MonoBehaviour
         }
         if (stunned == false)
         {
-
             try
             {
                 target = rangeCircle.GetComponent<RangeScript>().inRangeList[0];
-            } catch (ArgumentOutOfRangeException)
+            }
+            catch (ArgumentOutOfRangeException)
             {
-                Debug.Log("No Argument!");
+
             }
             timeUntilBullet -= Time.deltaTime;
             if (enemyInRange == true)
             {
                 if (timeUntilBullet < 0)
                 {
-                    GameObject Projectile = Instantiate(projectile, firePoint.position, Quaternion.Euler(firePoint.eulerAngles.x, firePoint.eulerAngles.y, firePoint.eulerAngles.z - 90));
-                    Projectile.GetComponent<ProjectileController>().target = target.transform;
-                    Projectile.GetComponent<ProjectileController>().speed = projectileSpeed / 10;
-                    timeUntilBullet = fireRate / 10;
+                    if (defaultTower == true)
+                    {
+                        GameObject Projectile = Instantiate(projectile, firePoint.position, Quaternion.Euler(firePoint.eulerAngles.x, firePoint.eulerAngles.y, firePoint.eulerAngles.z - 90));
+                        Projectile.transform.parent = bulletEmpty.transform;
+                        Projectile.GetComponent<ProjectileController>().target = target.transform;
+                        Projectile.GetComponent<ProjectileController>().speed = projectileSpeed / 10;
+                        timeUntilBullet = fireRate / 10;
+                    }
+                    if (IforgotTheName == true)
+                    {
+                        GameObject Projectile = Instantiate(projectile, firePoint.position, Quaternion.Euler(firePoint.eulerAngles.x, 100, firePoint.eulerAngles.z - 90));
+                        Projectile.GetComponent<ProjectileController>().target = target.transform;
+                        Projectile.GetComponent<ProjectileController>().speed = projectileSpeed / 10;
+                        timeUntilBullet = fireRate / 10;
+                    }
                 }
             }
             enemyInRange = rangeCircle.GetComponent<RangeScript>().enemyInRange;
