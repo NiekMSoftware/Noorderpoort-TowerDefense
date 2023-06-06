49db2ef448188faa9414c314cadabd109c94c63b
Niek Melet <niek+github@meletstudios.com>
2023-06-06 11:21:56 +0200

    Added Audio Player Pref

---
+++ TowerDefenseGame- Noorderpoort/Assets/Scripts/Audio/VolumeSettings.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [Header("Audio Mixer")]
+    [SerializeField] private AudioMixer audioMixer;
+    
+    [Header("Volume Sliders")]
+    [SerializeField] private Slider mainVolumeSlider;
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Slider sfxVolumeSlider;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("mainVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMainVolume();
+        }
+    }
+
+    public void SetMainVolume()
+    {
+        float volume = mainVolumeSlider.value;
+        audioMixer.SetFloat("Main Volume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("mainVolume", volume);
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = musicVolumeSlider.value;
+        audioMixer.SetFloat("Music Volume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = sfxVolumeSlider.value;
+        audioMixer.SetFloat("SFX Volume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        mainVolumeSlider.value = PlayerPrefs.GetFloat("mainVolume");
+        SetMainVolume();
+
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SetMainVolume();
+
+        sfxVolumeSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        SetSFXVolume();
+    }
+}
--- TowerDefenseGame- Noorderpoort/Assets/Scripts/Main Menu UI/ResolutionMenu.cs
+++ TowerDefenseGame- Noorderpoort/Assets/Scripts/Main Menu UI/ResolutionMenu.cs
@@ -13,28 +13,24 @@ namespace Settings
             switch (res)
             { 
                 case 0:
-                Screen.SetResolution(3840,2160,true);
+                    Screen.SetResolution(3840,2160,true);
+                    PlayerPrefs.Save();
                 break;
                 
                 case 1:
                 Screen.SetResolution(2560,1440,true);
+                PlayerPrefs.Save();
                 break;
                 
                 case 2:
                 Screen.SetResolution(1920,1080,true);
+                PlayerPrefs.Save();
                 break;
                 
                 case 3:
                 Screen.SetResolution(1280, 720, true);
+                PlayerPrefs.Save();
                 break;
-            }
-        }
-
-        private void FixedUpdate()
-        {
-            if (Input.GetKey(KeyCode.I))
-            {
-                Screen.SetResolution(2560,1440,true);
             }
         }
     }
