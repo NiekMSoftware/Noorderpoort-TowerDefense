963598cc79d49f9354e98a9afa4477f447ded70a
Niek Melet <niek+github@meletstudios.com>
2023-06-28 13:56:23 +0200

    map 2

--- TowerDefenseGame- Noorderpoort/Assets/Scripts/Level/SpawnInLevel.cs
+++ TowerDefenseGame- Noorderpoort/Assets/Scripts/Level/SpawnInLevel.cs
@@ -7,9 +7,16 @@ public class SpawnInLevel : MonoBehaviour
 {
     [SerializeField] private Transform locationLevel1;
     [SerializeField] private GameObject level1;
+    
+    [SerializeField] private Transform locationLevel2;
+    [SerializeField] private GameObject level2;
 
     public void SpawnLevel1()
     {
         Instantiate(level1, locationLevel1);
+    }
+
+    public void SpawnLevel2() {
+        Instantiate(this.level2, this.locationLevel2);
     }
 }
