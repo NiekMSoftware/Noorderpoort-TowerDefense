01cd4878e4711d827b122fc5866a9ea476a39bac
MK-v5 <mksomna@st.noorderpoort.nl>
2023-06-29 08:54:57 +0200

    yochurt
    arojsaodfnjlf

--- TowerDefenseGame- Noorderpoort/Assets/Scripts/Scripts (Martijn)/MainTowerHealth.cs
+++ TowerDefenseGame- Noorderpoort/Assets/Scripts/Scripts (Martijn)/MainTowerHealth.cs
@@ -6,6 +6,23 @@ public class MainTowerHealth : MonoBehaviour
 { 
      public float MTHealth;
 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Attacked()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<EnemyAttacking>() != null)
